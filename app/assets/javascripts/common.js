a08e0421cde8b9e55079abcf9ea003ffc868a5e5
Daniel Ávila <daniel@bitlab.mx>
2016-01-13 01:04:48 -0600

    Add navbar partial

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,5 +1,4 @@
 $(window).load(function() {
 	$('.menu').css("margin-top", $('.navbar').height() - $('.menu').height());
 	$('.search').css("margin-top", $('.navbar').height() - $('.search').height());
-	console.log($('.navbar').height() - $('.search').height());
 });
